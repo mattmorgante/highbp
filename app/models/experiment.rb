5fc6db3f1d1035c2b61b4cbda775a180114ff188
Matt <matthewmorgante@gmail.com>
2015-04-01 13:00:26 +0700

    server loads

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,12 +1,4 @@
 class Experiment < ActiveRecord::Base
   belongs_to :treatment
   belongs_to :user
-
-  def send_text_message 
-    twilio_sid = ENV["TWILIO_ACCOUNT_SID"]
-    twilio_auth_token = ENV["TWILIO_AUTH_TOKEN"]
-    twilio_phone_number = ENV["TWILIO_PHONE_NUMBER"]
-
-  end 
-
 end
